06d9a702a8ea7d3e0be3d228a9483f1cd7f38169
reechak <r.kansal9@gmail.com>
2018-09-10 12:54:58 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 # nonRepeating
-//Find Non repeating characters in a string
-
+# Find Non repeating characters in a string
+# Use Hashmap and Loop through each character. Add character as key and count of everytime the character appears as value .
 def non_repeating(given_string):
     char_count = {}
     for c in given_string:
